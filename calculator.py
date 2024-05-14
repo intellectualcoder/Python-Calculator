77b90a134f0bef5592c829c214c4cb83616d9b84
Sana <sanaq1992@live.com>
2024-05-13 23:30:04 -0500

    calculator iteration

--- calculator.py
+++ calculator.py
@@ -1,4 +1,17 @@
-x = int(input("what's x? "))
-y = int(input("what's y? "))
+#x = float(input("what's x? "))
+#y = float(input("what's y? "))
 
-print(x + y)
+#z = round(x + y)
+#z = round(x / y, 2)
+#z = x / y
+
+#print(f"{z:.2f}")
+
+def main():
+    x = int(input("whats x? "))
+    print("x squared is", square(x))
+
+def square(n) :
+        return n * n
+
+main()
